aef2989119c8d104be5b07541c03409462dee7ce
Akanksha <shahakanksha286@gmail.com>
2021-03-30 12:26:41 +0530

    added basic test

---
+++ one-one/test.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include "athread.h"
+#include <string.h>
+#include <unistd.h>
+
+#define print(str) write(1, str, strlen(str))
+
+void * start_routine(void * args){
+    print("Thread 1\n");
+    int * a = (int*)args;
+    
+    return (void*)a;
+}
+
+void * start_function(void * args){
+    print("Thread 2\n");
+    print("Thread 2 - exiting\n");
+
+    athread_exit((void*)1);
+}
+
+int main(){
+
+    athread_init();
+    
+    athread_t tid, tid2;
+    athread_attr_t attr;
+
+
+    athread_attr_init(&attr);
+    
+
+    int a = 10;
+    void * retval;
+
+    athread_create(&tid, &attr, start_routine, &a);
+    athread_create(&tid2, &attr, start_function, NULL);
+
+    sleep(10);
+    athread_join(tid, &retval);
+    printf("%d\n", *(int*)retval);
+    athread_join(tid2, &retval);
+    
+    return 0;
+}
