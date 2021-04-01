85b5922cc3f60b392c3d7809bcf5b14778b30f69
Akanksha <shahakanksha286@gmail.com>
2021-04-02 01:46:35 +0530

    added basic lock test

--- one-one/test.c
+++ one-one/test.c
@@ -5,68 +5,58 @@
 
 #define print(str) write(1, str, strlen(str))
 
-
-// void * start_routine(void * args){
-//     print("Thread 1\n");
-//     int * a = (int*)args;
-    
-//     return (void*)a;
-// }
-
-// void * start_function(void * args){
-//     print("Thread 2\n");
-//     print("Thread 2 - exiting\n");
-
-//     athread_exit((void*)1);
-// }
-
-// int main(){
-
-//     athread_init();
-    
-//     athread_t tid, tid2;
-//     athread_attr_t attr;
-
-
-//     athread_attr_init(&attr);
-    
-
-//     int a = 10;
-//     void * retval;
-
-//     athread_create(&tid, &attr, start_routine, &a);
-//     athread_create(&tid2, &attr, start_function, NULL);
-
-//     sleep(10);
-//     athread_join(tid, &retval);
-//     printf("%d\n", *(int*)retval);
-//     athread_join(tid2, &retval);
-    
-//     return 0;
-// }
-
-int c = 0;
+long int c = 0, c1 =0, c2 = 0, run = 1;
 void * f1(void *);
 void * f2(void *);
 
+athread_mutex_t mutex;
+
 void * f1(void * args){
-    print("f1\n");
+ 
+    while(run == 1){
+        
+        athread_mutex_lock(&mutex);
+        c1++;
+        c++;
+        athread_mutex_unlock(&mutex);
+       
+    }
     return NULL;
 }
 
 void * f2(void * args){
-    print("f2\n");
+    
+    while(run == 1){
+        
+        athread_mutex_lock(&mutex);
+        c2++;
+        c++;
+        athread_mutex_unlock(&mutex);
+       
+    }
     return NULL;
 }
 int main(){
+    
     athread_init();
 
     athread_t t1, t2;
 
+    athread_mutex_init(&mutex);
+
     athread_create(&t1, NULL, f1, NULL);
     athread_create(&t2, NULL, f2, NULL);
 
+    sleep(2);
+    run = 0;
+
     athread_join(t1, NULL);
     athread_join(t2, NULL);
+
+    printf("c = %ld  c1=%ld   c2 = %ld\n", c , c1, c2);
+
+    athread_mutex_destroy(&mutex);
+
+    return 0;
 
 }
