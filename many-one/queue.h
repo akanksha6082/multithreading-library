cab8b1e0dab33599f7b9fa2946504271665c947d
Akanksha <shahakanksha286@gmail.com>
2021-04-12 16:22:06 +0530

    added queue ADT

---
+++ many-one/queue.h
@@ -0,0 +1,30 @@
+#ifndef __QUEUE_H
+#define __QUEUE_H
+
+#include "athread_types.h"
+
+typedef struct node {
+    athread * tcb;
+    struct node * next;
+
+}node;
+
+
+typedef struct queue{
+    struct node * front;
+    struct node * rear;
+    int qcount;
+}queue;
+
+int qinit(queue *);
+node * create_node(athread *);
+void enqueue(queue *, athread *);
+node * dequeue(queue *);
+void display(queue *);
+athread * search_tcb(queue *, athread_t);
+int is_empty(queue *);
+int return_count(queue *);
+
+
+#endif
+
