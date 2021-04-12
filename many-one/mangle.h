1dc92933ca983a675b94352ef02a6cbeb168aafb
Akanksha <shahakanksha286@gmail.com>
2021-04-12 16:22:52 +0530

    added address encryption

---
+++ many-one/mangle.h
@@ -0,0 +1,20 @@
+#ifndef __MANGLE_H
+#define __MANGLE_H
+
+#define JB_SP 6
+#define JB_PC 7
+
+static long int i64_ptr_mangle(long int p){
+    long int ret;
+    asm(" mov %1, %%rax;\n"
+        " xor %%fs:0x30, %%rax;"
+        " rol $0x11, %%rax;"
+        " mov %%rax, %0;"
+    : "=r"(ret)
+    : "r"(p)
+    : "%rax"
+    );
+    return ret;
+}
+
+#endif
