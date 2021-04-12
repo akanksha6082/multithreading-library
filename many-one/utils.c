3e568dedf3eb08c3ce36c7ba629376e3ce199ba9
Akanksha <shahakanksha286@gmail.com>
2021-04-12 16:20:40 +0530

    added helper functions

---
+++ many-one/utils.c
@@ -0,0 +1,18 @@
+#include <sys/resource.h>
+#include <unistd.h>
+
+
+#include "utils.h"
+
+_uint get_stack_limit(){
+    
+    struct rlimit rlimit;
+    int resource = RLIMIT_STACK;
+    getrlimit(resource, &rlimit);
+    return rlimit.rlim_cur;
+}
+
+_uint get_page_size(){
+    
+    return sysconf(_SC_PAGESIZE);
+}
